eaddb8cb814ab2139bba168e6702a1ea2370d140
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2024-08-31 18:53:15 -0700

    Add comprehensive unit tests for BlazorApp components
    Introduced new unit tests for various components and models in the BlazorApp project to ensure functionality and correctness.
    
    - Added `FakeUserTests` to validate `FakeUser` class behavior.
    - Added `UserInfoTests` to validate `UserInfo` component behavior.
    - Added `UserRightsTests` to validate `UserRights` component behavior.
    - Added `MainLayoutTests` to validate `MainLayout` component display.
    - Added `NavMenuTests` to validate `NavMenu` component display.
    - Added `BasicUserModelTests` to validate `BasicUserModel` class.
    
    These tests cover different states and scenarios, improving the reliability and maintainability of the application.

---
+++ src/BlazorApp.Shared.Unit.Tests/Components/UserRightsTests.cs
@@ -0,0 +1,183 @@
+﻿// ============================================
+// Copyright (c) 2024. All rights reserved.
+// File Name :     UserRightsTests.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : BlazorApp
+// Project Name :  BlazorApp.Shared.Unit.Tests
+// =============================================
+
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Claims;
+
+namespace BlazorApp.Shared.Components;
+
+[ExcludeFromCodeCoverage]
+public class UserRightsTests : TestContext
+{
+	[Fact]
+	public void UserRights_With_ASingleRole_Should_DisplayTheResult_Test()
+	{
+		// Arrange
+
+		const string expected =
+			"""
+			<h1>Hi TEST USER, you have these claims and rights:</h1>
+			<ul>
+				<li>You have the role USER</li>
+			</ul>
+			""";
+
+		var authContext = this.AddTestAuthorization();
+		authContext.SetAuthorized("TEST USER");
+		authContext.SetRoles("user");
+
+		// Act
+
+		var cut = RenderComponent<UserRights>();
+
+		// Assert
+
+		cut.MarkupMatches(expected);
+	}
+
+	[Fact]
+	public void UserRights_With_MultipleRoles_Should_DisplayTheResult_Test()
+	{
+		// Arrange
+
+		const string expected =
+			"""
+			<h1>Hi TEST USER, you have these claims and rights:</h1>
+			<ul>
+				<li>You have the role USER</li>
+				<li>You have the role ADMIN</li>
+			</ul>
+			""";
+
+		var authContext = this.AddTestAuthorization();
+		authContext.SetAuthorized("TEST USER");
+		authContext.SetRoles("admin", "user");
+
+		// Act
+
+		var cut = RenderComponent<UserRights>();
+
+		// Assert
+
+		cut.MarkupMatches(expected);
+	}
+
+	[Fact]
+	public void UserRights_With_ASinglePolicy_Should_DisplayTheResult_Test()
+	{
+		// Arrange
+
+		const string expected =
+			"""
+			<h1>Hi TEST USER, you have these claims and rights:</h1>
+			<ul>
+			  <li>You are a CONTENT EDITOR</li>
+			</ul>
+			""";
+
+		var authContext = this.AddTestAuthorization();
+		authContext.SetAuthorized("TEST USER");
+		authContext.SetPolicies("content-editor");
+
+		// Act
+
+		var cut = RenderComponent<UserRights>();
+
+		// Assert
+
+		cut.MarkupMatches(expected);
+	}
+
+	[Fact()]
+	public void UserRights_With_MultipleClaimTypes_Should_DisplayTheResult_Test()
+	{
+		// Arrange
+
+		const string expected =
+			"""
+			<h1>Hi TEST USER, you have these claims and rights:</h1>
+			<ul>
+				<li>Emailaddress: test@example.com</li>
+				<li>Dateofbirth: 01-01-1970</li>
+			</ul>
+			""";
+
+		var authContext = this.AddTestAuthorization();
+		authContext.SetAuthorized("TEST USER");
+		authContext.SetClaims(
+			new Claim(ClaimTypes.Email, "test@example.com"),
+			new Claim(ClaimTypes.DateOfBirth, "01-01-1970")
+		);
+
+		// Act
+
+		var cut = RenderComponent<UserRights>();
+
+		// Assert
+
+		cut.MarkupMatches(expected);
+	}
+
+	[Fact]
+	public void UserRights_With_ClaimsRolesAndPolicies_Should_DisplayTheResult_Test()
+	{
+		// Arrange
+
+		const string expected =
+			"""
+			<h1>Hi TEST USER, you have these claims and rights:</h1>
+			<ul>
+				<li>Emailaddress: test@example.com</li>
+				<li>You have the role USER</li>
+				<li>You have the role ADMIN</li>
+				<li>You are a CONTENT EDITOR</li>
+			</ul
+			""";
+
+		var authContext = this.AddTestAuthorization();
+		authContext.SetAuthorized("TEST USER");
+		authContext.SetRoles("admin", "user");
+		authContext.SetPolicies("content-editor");
+		authContext.SetClaims(new Claim(ClaimTypes.Email, "test@example.com"));
+
+		// Act
+
+		var cut = RenderComponent<UserRights>();
+
+		// Assert
+
+		cut.MarkupMatches(expected);
+	}
+
+	[Fact()]
+	public void UserRights_With_SetAuthenticationType_Should_DisplayTheType_Test()
+	{
+		// Arrange
+
+		const string expected =
+			"""
+			<h1>Hi TEST USER, you have these claims and rights:</h1>
+			<ul>
+				<li>You have the authentication type CUSTOM AUTH TYPE</li>
+			</ul>
+			""";
+
+		var authContext = this.AddTestAuthorization();
+		authContext.SetAuthorized("TEST USER");
+		authContext.SetAuthenticationType("custom-auth-type");
+
+		// Act
+
+		var cut = RenderComponent<UserRights>();
+
+		// Assert
+
+		cut.MarkupMatches(expected);
+	}
+}
---
+++ src/BlazorApp.Shared.Unit.Tests/Models/BasicUserModelTests.cs
@@ -0,0 +1,59 @@
+﻿// ============================================
+// Copyright (c) 2024. All rights reserved.
+// File Name :     BasicUserModelTests.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : BlazorApp
+// Project Name :  BlazorApp.Shared.Unit.Tests
+// =============================================
+
+using System.Diagnostics.CodeAnalysis;
+using BlazorApp.Shared.BogusFakes;
+using FluentAssertions;
+
+namespace BlazorApp.Shared.Models;
+
+[ExcludeFromCodeCoverage]
+public class BasicUserModelTests
+{
+	[Fact()]
+	public void BasicUserModel_With_AUserInfo_Should_ReturnAValidBasicUserModel_Test()
+	{
+		//Arrange
+
+		var user = FakeUser.GetNewUser(true);
+
+		//Act
+
+		var result = new BasicUserModel(user);
+
+
+		//Assert
+
+		result.Should().NotBeNull();
+		result.UserName.Should().Be(user.UserName);
+		result.UserId.Should().Be(user.UserId);
+		result.Email.Should().Be(user.Email);
+		result.Roles.Should().BeEquivalentTo(user.Roles);
+	}
+
+	[Fact()]
+	public void BasicUserModel_With_ValuesFromAUserInfo_Should_Return_AValidBasicUserModel_Test()
+	{
+		//Arrange
+
+		var user = FakeUser.GetNewUser(true);
+
+		//Act
+
+		var result = new BasicUserModel(user.UserName, user.UserId, user.Email, user.Roles);
+
+		//Assert
+
+		result.Should().NotBeNull();
+		result.UserName.Should().Be(user.UserName);
+		result.UserId.Should().Be(user.UserId);
+		result.Email.Should().Be(user.Email);
+		result.Roles.Should().BeEquivalentTo(user.Roles);
+	}
+}
