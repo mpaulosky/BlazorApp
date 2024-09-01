be6ed25587562f45e41173b56175047766078ac7
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2024-08-31 18:51:58 -0700

    Configure unit testing with Bunit and xUnit
    Updated BlazorApp.Shared.Unit.Tests.csproj to target .NET 8.0, enable nullable reference types, and add necessary package references for unit testing with Bunit and xUnit. Added project references to BlazorApp.Client and BlazorApp.Shared. Created a folder for services and set the root namespace to BlazorApp.Shared.
    
    Added LoginStatusTests.cs to test the LoginStatus component's link display based on user authentication status.
    
    Added LoginTests.cs to test the Login component's login and logout actions using a mock ILoginProvider.
    
    Updated _Imports.razor to include using directives for Blazor, Bunit, and xUnit namespaces.

---
+++ src/BlazorApp.Shared.Unit.Tests/Security/LoginTests.cs
@@ -0,0 +1,83 @@
+﻿// ============================================
+// Copyright (c) 2024. All rights reserved.
+// File Name :     LoginTests.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : BlazorApp
+// Project Name :  BlazorApp.Shared.Unit.Tests
+// =============================================
+
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared.Security;
+
+[ExcludeFromCodeCoverage]
+public class LoginTests : TestContext
+{
+	[Fact]
+	public void Login_With_LoginProvider_Should_InvokesLoginProvider_Test()
+	{
+		// Arrange
+
+		this.AddTestAuthorization();
+
+
+		var loginProvider = new LoginProviderMock();
+
+		Services.AddSingleton<ILoginProvider>(loginProvider);
+
+		// Act
+
+		var cut = RenderComponent<Login>();
+
+		cut.Find("a").Click();
+
+		// Assert
+
+		Assert.True(loginProvider.LoginAsyncInvoked);
+	}
+
+	[Fact]
+	public void Logout_With_LoginProvider_Should_InvokesLoginProvider_Test()
+	{
+		// Arrange
+
+		var authContext = this.AddTestAuthorization();
+		authContext.SetAuthorized("TEST USER");
+
+
+		var loginProvider = new LoginProviderMock();
+
+		Services.AddSingleton<ILoginProvider>(loginProvider);
+
+		// Act
+
+		var cut = RenderComponent<Login>();
+
+		cut.Find("a").Click();
+
+		// Assert
+
+		Assert.True(loginProvider.LogoutAsyncInvoked);
+	}
+}
+
+[ExcludeFromCodeCoverage]
+public class LoginProviderMock : ILoginProvider
+{
+	public bool LoginAsyncInvoked { get; private set; }
+	public bool LogoutAsyncInvoked { get; private set; }
+
+	public Task LoginAsync()
+	{
+		LoginAsyncInvoked = true;
+		return Task.CompletedTask;
+	}
+
+	public Task LogoutAsync()
+	{
+		LogoutAsyncInvoked = true;
+		return Task.CompletedTask;
+	}
+}
