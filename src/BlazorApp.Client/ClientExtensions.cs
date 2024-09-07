f0c66cfcb85e81f52cd193051731dee9cad26270
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2024-09-06 21:00:34 -0700

    Refactor service configuration in Blazor app
    Introduce `ClientExtensions.cs` with `ConfigureClientServices` method to centralize client service configuration. Update `Program.cs` to use this method, improving maintainability. Add metadata to the new file.

---
+++ src/BlazorApp.Client/ClientExtensions.cs
@@ -0,0 +1,38 @@
+﻿// ============================================
+// Copyright (c) 2024. All rights reserved.
+// File Name :     ClientExtensions.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : BlazorApp
+// Project Name :  BlazorApp.Client
+// =============================================
+
+using System.Diagnostics.CodeAnalysis;
+
+using BlazorApp.Shared.Security;
+using BlazorApp.Shared.Services;
+
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BlazorApp.Client;
+
+/// <summary>
+///  Client extensions.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public static class ClientExtensions
+{
+	/// <summary>
+	/// Configure client services.
+	/// </summary>
+	/// <param name="builder"></param>
+	public static void ConfigureClientServices(this WebAssemblyHostBuilder builder)
+	{
+		builder.Services.AddAuthorizationCore();
+		builder.Services.AddCascadingAuthenticationState();
+		builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+		builder.Services.AddSingleton<IBlazorTestService, ClientTestService>();
+		builder.Services.AddSingleton<ILoginProvider, WebLoginProvider>();
+	}
+}
