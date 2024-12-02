f54af629dacefc29d9cf6bab41ce13e6e8051f32
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2024-12-02 09:12:35 -0800

    Add Redis and MongoDB resources to BlazorApp
    Introduce Redis for output caching and MongoDB for data storage with a management interface. Update the project to include external HTTP endpoints and integrate the newly added resources.

--- src/BlazorApp.AppHost/Program.cs
+++ src/BlazorApp.AppHost/Program.cs
@@ -1,3 +1,16 @@
-var builder = DistributedApplication.CreateBuilder(args);
+using static BlazorApp.AppHost.Constants.ServiceNames;
+
+IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
+
+IResourceBuilder<RedisResource> cache = builder.AddRedis(OutputCache);
+
+var db = builder.AddMongoDB("db")
+	.WithMongoExpress()
+	.AddDatabase(MongoDbName);
+
+builder.AddProject<Projects.BlazorApp>("web-frontend")
+	.WithExternalHttpEndpoints()
+	.WithReference(cache)
+	.WithReference(db);
 
 builder.Build().Run();
